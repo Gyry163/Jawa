062748ef4f37fbba65d9b7627dcdd0538b71c56f
Gyry163 <faq163m@gmail.com>
2021-08-08 02:27:22 +0400

    мутирование

---
+++ app.js
@@ -0,0 +1,20 @@
+// 1 Переменные
+const firstName = 'Maksimka'// Приоритет
+var name = 'Maxilen' // Не использовать
+let age = 25  
+
+name = 'maks'
+console.log(name)
+
+
+const _private = 'private'
+const $ = 'some value'
+const with5 = '5'
+//const 5with = '5' err
+
+//2 Мутирование
+console.log('Имя человека:' + firstName + ', возраст:' + age)
+const lastMame = prompt('Введите фамилию')
+alert(firstName + ' ' + lastMame)
+
+//Операторы
